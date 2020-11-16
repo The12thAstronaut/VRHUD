6422221c8fc6f19bc9363fb581a8e79cdb4d1af0
Neil McHenry <neilmchenry@gmail.com>
2020-11-15 19:28:53 -0600

    Update sceneLoader.cs

--- SpaceProject_final/Assets/Scripts/sceneLoader.cs
+++ SpaceProject_final/Assets/Scripts/sceneLoader.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Input;
 using UnityEngine.SceneManagement;
 
 public class sceneLoader : MonoBehaviour
@@ -16,41 +15,37 @@ public class sceneLoader : MonoBehaviour
     void Update()
     {
         //If the left side of the VIVE left/right controller trackpad is pressed, load scene
-        if(Input.GetKeyDown(
-                                KeyCode.Y
-                                || (Input.GetAxis("LeftVIVETrackpadHorizontal") == -1.0) 
-                                || (Input.GetAxis("RightVIVETrackpadHorizontal") == -1.0)
-                            ))
+        if(Input.GetKeyDown(KeyCode.Y)
+            || (Input.GetAxis("LeftVIVETrackpadHorizontal") == -1.0) 
+            || (Input.GetAxis("RightVIVETrackpadHorizontal") == -1.0)
+            )
         {
               SceneManager.LoadScene("moonScene_Gaze", LoadSceneMode.Single);
         }
 
         //If the top side of the VIVE left controller trackpad is pressed, load scene
-        if(Input.GetKeyDown(
-                                KeyCode.U
-                                || (Input.GetAxis("LeftVIVETrackpadVertical") == 1.0)
-                                || (Input.GetAxis("RightVIVETrackpadVertical") == 1.0)
-                            ))
+        if(Input.GetKeyDown(KeyCode.U)
+            || (Input.GetAxis("LeftVIVETrackpadVertical") == 1.0)
+            || (Input.GetAxis("RightVIVETrackpadVertical") == 1.0)
+            )
         {
               SceneManager.LoadScene("moonScene_Eyetracking", LoadSceneMode.Single);
         }
 
         //If the right side of the VIVE left/right controller trackpad is pressed, load scene
-        if(Input.GetKeyDown(
-                                KeyCode.I
-                                || (Input.GetAxis("LeftVIVETrackpadHorizontal") == 1.0) 
-                                || (Input.GetAxis("RightVIVETrackpadHorizontal") == 1.0)
-                            ))
+        if(Input.GetKeyDown(KeyCode.I)
+            || (Input.GetAxis("LeftVIVETrackpadHorizontal") == 1.0) 
+            || (Input.GetAxis("RightVIVETrackpadHorizontal") == 1.0)
+            )
         {
               SceneManager.LoadScene("moonScene_Gesture", LoadSceneMode.Single);
         }
 
         //If the bottom side of the VIVE left controller trackpad is pressed, load scene
-        if(Input.GetKeyDown(
-                                KeyCode.O
-                                || (Input.GetAxis("LeftVIVETrackpadVertical") == -1.0)
-                                || (Input.GetAxis("RightVIVETrackpadVertical") == -1.0)
-                            ))
+        if(Input.GetKeyDown(KeyCode.O)
+                || (Input.GetAxis("LeftVIVETrackpadVertical") == -1.0)
+                || (Input.GetAxis("RightVIVETrackpadVertical") == -1.0)
+            )
         {
               SceneManager.LoadScene("moonScene_Voice", LoadSceneMode.Single);
         }
