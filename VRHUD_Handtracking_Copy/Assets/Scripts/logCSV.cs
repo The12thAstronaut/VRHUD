e36b50a5425e6b94759d7f0ce3f01b625deb67cc
Neil McHenry <neilmchenry@gmail.com>
2020-11-25 11:19:30 -0600

    Update logCSV.cs

--- VRHUD_Handtracking_Copy/Assets/Scripts/logCSV.cs
+++ VRHUD_Handtracking_Copy/Assets/Scripts/logCSV.cs
@@ -6,12 +6,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 using UnityEngine.SceneManagement;
 
 public class logCSV : MonoBehaviour
 {
     private string currentTimeString;
-    public string timeDifference;
+    private List<string> data_sceneCommand;
+
+    public float timeDifference;
+    public string timeDifferenceString;
 
     public float currentTime;    
     public float pastTime;
@@ -23,21 +28,60 @@ public class logCSV : MonoBehaviour
 
     public int trialNumber;
     public string trialNumberRef;
+    public TMP_InputField InputField; 
+
+    public List<string> data_participantID;
+    public List<string> data_currentTime;
+    public List<string> data_timeDifference;
+    public List<string> data_sceneName;
+    public List<string> data_trialNumber;
+
+    public string[] csvFiles;
+    public string recentCSV;
+
 
     void Awake()
     {   
         //Makes sure that all the data is together
         DontDestroyOnLoad(this.gameObject);
+
+        //Read most recent CSV file data
+            //Search for CSV files within project directory
+            csvFiles = System.IO.Directory.GetFiles(System.IO.Directory.GetCurrentDirectory(), "*.csv");
+
+            //Find most recent CSV file and save the string to the recentCSV variable
+            var files = new DirectoryInfo(System.IO.Directory.GetCurrentDirectory()).GetFiles("*.csv");
+            DateTime lastModified = DateTime.MinValue;
+            foreach (FileInfo file in files)
+            {
+                if (file.LastWriteTime > lastModified)
+                {
+                    lastModified = file.LastWriteTime;
+                    recentCSV = file.Name;
+                }
+            }
+
+            //Initialize data_participantID array's first and second element to null
+            data_participantID.Add("null");
+            data_participantID.Add("null");
+
+            //Call the readCSV function and use the most recent CSV as an input
+            readCSV(recentCSV);
+
+        //Read CSV file data, hardcoded option
+            // string CSVPath = @"C:\Users\nmchenry1\Documents\GitHub\VRHUD\SpaceProject_final\12_VRHUD_Task_Time.csv";
+            // readCSV(CSVPath);
     }
 
     // Start is called before the first frame update
     void Start()
     {
         pastTime = 0.0f;
-        //Add a header line to the csv file
-        addRecord("participantID", "currentTimeString", "timeDifference", "sceneName", "sceneNumber", participantID + "_VRHUD_Task_Time.csv");
         trialNumber = -1;
+        // InputField.onValueChanged.AddListener(delegate {ValueChangeCheck(); });
 
+        //Loads the scene specified in the commands.csv
+        loadCommandedScene("commands/commands.csv");
     }
 
     // Update is called once per frame
@@ -65,8 +109,17 @@ public class logCSV : MonoBehaviour
         //Only record data if the space bar is pressed on the keyboard
         if(Input.GetKeyDown(KeyCode.Space))
         {
-            timeDifference = (currentTime - pastTime).ToString("f6");
-            addRecord(participantID, currentTimeString, timeDifference, sceneName, trialNumberRef, participantID + "_VRHUD_Task_Time.csv");
+            timeDifference = (currentTime - pastTime);
+            int min_diff = Mathf.FloorToInt(timeDifference/60);
+            int sec_diff = Mathf.FloorToInt(timeDifference%60);
+            timeDifferenceString = min_diff.ToString("00") + ":" + sec_diff.ToString("00");     //Save game time in seconds as a string
+            //Add a header line to the csv file, but only if that file is new and a past participant number exists in the most recent CSV file
+                if(data_participantID[1] != participantID)
+                {
+                    addRecord("participantID", "currentTimeString", "timeDifferenceString", "sceneName", "trialNumber", participantID + "_VRHUD_Task_Time.csv");
+                }
+            //Add data as a new line to csv file
+            addRecord(participantID, currentTimeString, timeDifferenceString, sceneName, trialNumberRef, participantID + "_VRHUD_Task_Time.csv");
             Debug.Log("Time: " + currentTimeString + "logged to CSV");
             pastTime = currentTime;
         }
@@ -101,4 +154,98 @@ public class logCSV : MonoBehaviour
             throw new ApplicationException("This program did an oopsie :", ex);
         }
     }
+
+    public void ValueChangeCheck()
+    {
+        // participantID = InputField.GetComponent<TMP_InputField>().text;
+        Debug.Log("Value Changed");
+    }
+
+    //Function that reads CSV file
+    public void readCSV(string csvPath)
+    {
+        //Read in CSV file specified by csvPath input variable
+        using(var reader = new StreamReader(csvPath))
+        {
+            //Create a different string list for each CSV column
+            List<string> listA = new List<string>();
+            List<string> listB = new List<string>();
+            List<string> listC = new List<string>();
+            List<string> listD = new List<string>();
+            List<string> listE = new List<string>();
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                var values = line.Split(',');
+
+                listA.Add(values[0]);
+                listB.Add(values[1]);
+                listC.Add(values[2]);
+                listD.Add(values[3]);
+                listE.Add(values[4]);
+
+                data_participantID = listA;
+                data_currentTime = listB;
+                data_timeDifference = listC;
+                data_sceneName = listD;
+                data_trialNumber = listE;
+            }
+        }
+    }
+
+    //Function that reads the "commands.csv" CSV file and loads the last scene listed
+    public void loadCommandedScene(string commandsPath)
+    {
+        //Read in CSV file specified by csvPath input variable
+        using(var reader = new StreamReader(commandsPath))
+        {
+            //Create a different string list for each CSV column
+            List<string> listTemp = new List<string>();
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                var values = line.Split(',');
+                listTemp.Add(values[0]);
+                data_sceneCommand = listTemp;
+            }
+        }
+
+        //Save last command CSV array element to string
+        string lastElement = data_sceneCommand[data_sceneCommand.Count - 1];
+
+        //Check to see if the command has been executed or not yet. If not, load the scene and add the string "Finished"
+            if(lastElement != "Finished")
+            {
+                addFinishedCommand("commands/commands.csv");
+                //Loads the scene stored in the second to last element of the data_sceneCommmand list
+                Debug.Log("Loading " + lastElement + "...");
+                SceneManager.LoadScene(lastElement, LoadSceneMode.Single);
+            }
+
+        //If most recent scene was the pop-up menu, open the scene commanded by the pop-up scene
+            // if(data_sceneName[data_sceneName.Count - 1] == "moonScene_Eyetracking")
+            // {
+            //     Debug.Log("Loading Popup Scene");
+            // }
+        //Else do nothing
+    }
+
+    public static void addFinishedCommand(string filepath)
+    {
+        try
+        {
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(@filepath, true))
+            {
+                //Save data as a new line in CSV file
+                file.WriteLine("Finished" + ",");
+            }
+        }
+        catch(Exception ex)
+        {
+            throw new ApplicationException("This program did an oopsie :", ex);
+        }
+    }
+
+    //Work In Progress:
+    //Read the participant ID and set it when the scene is reopened
 }
